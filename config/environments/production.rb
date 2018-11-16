30aa759e9b6ff77c2d8a5e72c9c6a4d9986205b6
sachin nandha sabarish <sachinjeevan2506@gmail.com>
2018-11-17 00:27:42 +0530

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      user_name:      ENV['sachinjeevan2506@gmail.com'],
-      password:       ENV['sachin2903'],
-      domain:         ENV['learnlykengineer.herokuapp.com'],
+      user_name:      'sachinjeevan2506@gmail.com',
+      password:       'sachin2903',
+      domain:         'learnlykengineer.herokuapp.com',
       address:       'smtp.gmail.com',
       port:          '587',
-      authentication: :plain,
+      authentication: 'plain',
       enable_starttls_auto: true
 }
   if ENV["RAILS_LOG_TO_STDOUT"].present?
