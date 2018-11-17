ce75fcb10f9a799e40ae2c731e6e5d5f51259320
sachinjeevan <sachinjeevan2506@gmail.com>
2018-11-17 17:16:31 +0000

    hh

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
          
          def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  where( uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     # If you are using confirmable and the provider(s) you use validate emails, 
