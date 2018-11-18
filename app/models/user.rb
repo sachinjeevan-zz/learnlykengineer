0411beb27a71d5cfa7c88cc624a8b5b38099fc60
sachin nandha sabarish <sachinjeevan2506@gmail.com>
2018-11-19 00:04:00 +0530

    kk

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :trackable,
+  devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
          def self.new_with_session(params, session)
