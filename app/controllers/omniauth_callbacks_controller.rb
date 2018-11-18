8e24ab1c6ed3e880b0ed83b98b77457d9ea1aa4e
sachin nandha sabarish <sachinjeevan2506@gmail.com>
2018-11-18 19:07:29 +0530

    rake

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,19 +1,18 @@
-class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def self.callback_for(provider)
-    class_eval %{
-      def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"],current_user)
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-        else
-          session["devise.#{provider}_data"] = env["omniauth.auth"]
-          redirect_to new_user_registration_url
-        end
-      end
-    }
-end
-[:faceboook].each do |provider|
-  callback_for provider
-end
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
 end
