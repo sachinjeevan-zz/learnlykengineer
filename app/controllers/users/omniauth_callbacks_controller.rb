679db149b8278e267f4301a17cce79620f48c79b
sachin nandha sabarish <sachinjeevan2506@gmail.com>
2018-11-19 11:27:54 +0530

    fix fb bug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,13 +2,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
+    
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      @user.remember_me = true
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
+      @user.remember_me = true
       redirect_to new_user_registration_url
     end
-  end
+    
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
         def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
           data = access_token.extra.raw_info
-          if user = User.where(:email => data.email).first
+          if user = User.where(:email => data.email)
             user
           else # Create a user with a stub password. 
             user = User.new(:email => data.email, :password => Devise.friendly_token[0,20])
